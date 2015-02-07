2f46597de24e90280a0ffe77652a376bf021096e
Uri Shatil <uri.shatil@gmail.com>
2015-02-07 13:57:30 -0800

    Upgrade to Python 3 with Django 1.7

--- ws/wellspring/urls.py
+++ ws/wellspring/urls.py
@@ -1,20 +1,12 @@
 from django.conf.urls import patterns, include, url
-from views.hello import hello
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+from wellspring.views.hello import hello
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'wellspring.views.home', name='home'),
-    # url(r'^wellspring/', include('wellspring.foo.urls')),
+    # url(r'^blog/', include('blog.urls')),
+	url(r'wellspring/hello', hello, name='hello'),
 
-	url(r'wellspring/hello', hello, name='hello')
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls))
 )
