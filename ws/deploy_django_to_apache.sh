f2266ce71651f3fb4fa9282f2bbefa1f0379af89
Uri Shatil <uri.shatil@gmail.com>
2015-04-19 16:29:49 -0400

    Added some utility scripts

---
+++ ws/deploy_django_to_apache.sh
@@ -0,0 +1,3 @@
+sudo rm -r /var/www/wellspring/*
+sudo cp -r /work/wellness-tracker/ws/wellspring /var/www/wellspring
+sudo service httpd restart
---
+++ ws/deploy_static_to_apache.sh
@@ -0,0 +1,2 @@
+sudo cp -r /work/wellness-tracker/mobile/www/* /var/www/html/
+sudo service httpd restart
